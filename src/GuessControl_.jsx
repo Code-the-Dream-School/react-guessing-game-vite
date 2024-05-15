bc8eb342242263310df5558a916c788846ce5cc4
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-05-14 19:24:58 -0700

    change class components to function

---
+++ src/GuessControl_.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Button from "./Button";
+
+function GuessControl({ onGuess }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  const handleInputChange = (event) => {
+    setCurrentGuess(event.target.value);
+  };
+
+  const onSubmitGuess = () => {
+    onGuess(Number(currentGuess));
+    setCurrentGuess("");
+  };
+
+  return (
+    <div>
+      <input type="number" value={currentGuess} onChange={handleInputChange} />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
+}
+
+export default GuessControl;
