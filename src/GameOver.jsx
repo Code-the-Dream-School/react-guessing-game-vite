dc1b90e559a360de009ddcb2bd4848f0900fd65f
Alzhan-B <alzhan.braliyev@gmail.com>
2024-09-04 20:56:50 -0400

    Lesson_2_1 React Router Class Components

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -1,10 +1,12 @@
 import Button from "./Button";
 
+// eslint-disable-next-line react/prop-types
 function GameOver({ hasWon, onReset }) {
   return (
     <div>
       {hasWon && <h2>Congratulation! You guessed my number.</h2>}
       {!hasWon && (
+        // eslint-disable-next-line react/no-unescaped-entities
         <h2>You didn't guess my number. Would you like to try again?</h2>
       )}
       <Button onClick={onReset}>Play Again!</Button>
