4047d506389c3bd38c0f08f9a559a8a95e0d56f3
Anna Bobrova <spomandarin@gmail.com>
2025-01-21 02:04:35 -0600

    Lesson 12 code added.

--- src/GuessControl.jsx
+++ src/GuessControl.jsx
@@ -1,47 +1,77 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
-class GuessControl extends Component {
-  constructor(props) {
-    super(props);
+// // // class GuessControl extends Component {
+// // // 12.1 - Rename the current GuessControl class to GuessControlOld if you want to keep it a reference while converting the code
+// export class GuessControlOld extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      currentGuess: "",
-    };
+//     this.state = {
+//       currentGuess: "",
+//     };
 
-    /**
-     * These lines are required to make the methods/functions declared on this
-     *  class have the correct `this` object when they run.
-     */
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.onSubmitGuess = this.onSubmitGuess.bind(this);
-  }
+//     /**
+//      * These lines are required to make the methods/functions declared on this
+//      *  class have the correct `this` object when they run.
+//      */
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.onSubmitGuess = this.onSubmitGuess.bind(this);
+//   }
 
-  handleInputChange(event) {
-    this.setState({ currentGuess: event.target.value });
-  }
+//   handleInputChange(event) {
+//     this.setState({ currentGuess: event.target.value });
+//   }
 
-  onSubmitGuess() {
-    // Since the values from an HTML input are strings by default,
-    //  convert to a number for the returned guess value
-    //  by passing in the string to the Number function.
-    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-    this.props.onGuess(Number(this.state.currentGuess));
-    this.setState({ currentGuess: "" });
-  }
+//   onSubmitGuess() {
+//     // Since the values from an HTML input are strings by default,
+//     //  convert to a number for the returned guess value
+//     //  by passing in the string to the Number function.
+//     // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+//     this.props.onGuess(Number(this.state.currentGuess));
+//     this.setState({ currentGuess: "" });
+//   }
 
-  render() {
-    return (
-      <div>
-        <input
-          type="number"
-          value={this.state.currentGuess}
-          onChange={this.handleInputChange}
-        />
-        <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <input
+//           type="number"
+//           value={this.state.currentGuess}
+//           onChange={this.handleInputChange}
+//         />
+//         <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
+//       </div>
+//     );
+//   }
+// }
+
+// // export default GuessControl;
+
+// 12.2 - Create a new function component called GuessControl that will take an onGuess prop
+const GuessControl = ({ onGuess }) => {
+  // 12.4 - Create a state with useState
+  const [currentGuess, setCurrentGuess] = useState("");
+
+  // 12.5 - Input change handler (Create a handleInputChange function within the component that updates the currentGuess state value when the user changes the value in the input. Set the onChange property for the input element to refer to this function.)
+  const handleInputChange = (event) => {
+    setCurrentGuess(event.target.value);
+  };
+
+  // 12.6 - Guess send handler - Create a onSubmitGuess function that calls the onGuess prop with the currentGuess value converted to a number and also resets the currentGuess to an empty string when it is called
+  const onSubmitGuess = () => {
+    onGuess(Number(currentGuess)); // Pass the current value to the onGuess prop
+    setCurrentGuess(""); // Resetting the state
+  };
+
+  // 12.3 - Copy the return value from the render function in the class component to be the return value in the new function component. Remove any references to this. since those will be replaced with new references to local variables or props passed in to the function component
+  return (
+    <div>
+      <input type="number" value={currentGuess} onChange={handleInputChange} />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
+};
 
 export default GuessControl;
--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -1,77 +1,125 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import GuessControl from "./GuessControl";
 import GuessMessage from "./GuessMessage";
 import GameOver from "./GameOver";
 
-/**
- *
- * Returns a random integer number from 1-100 inclusive
- */
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
+// /**
+//  *
+//  * Returns a random integer number from 1-100 inclusive
+//  */
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * 100) + 1;
+// }
 
-const MAX_ATTEMPTS = 5;
+// const MAX_ATTEMPTS = 5;
 
-class NumberGuessingGame extends Component {
-  constructor(props) {
-    super(props);
+// // class NumberGuessingGame extends Component {
+// // 12.7 - Rename the current NumberGuessingGame class to NumberGuessingGameOld if you want to keep it a reference while converting the code
+// export class NumberGuessingGameOld extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      numberToGuess: getRandomNumber(),
-      numberOfGuesses: 0,
-      latestGuess: null,
-    };
+//     this.state = {
+//       numberToGuess: getRandomNumber(),
+//       numberOfGuesses: 0,
+//       latestGuess: null,
+//     };
 
-    /**
-     * These lines are required to make the methods/functions declared on this
-     *  class have the correct `this` object when they run.
-     */
-    this.handleGuess = this.handleGuess.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
+//     /**
+//      * These lines are required to make the methods/functions declared on this
+//      *  class have the correct `this` object when they run.
+//      */
+//     this.handleGuess = this.handleGuess.bind(this);
+//     this.handleReset = this.handleReset.bind(this);
+//   }
 
-  handleGuess(guess) {
-    this.setState({
-      latestGuess: guess,
-      numberOfGuesses: this.state.numberOfGuesses + 1,
-    });
-  }
+//   handleGuess(guess) {
+//     this.setState({
+//       latestGuess: guess,
+//       numberOfGuesses: this.state.numberOfGuesses + 1,
+//     });
+//   }
 
-  handleReset() {
-    this.setState({
-      numberToGuess: getRandomNumber(),
-      numberOfGuesses: 0,
-      latestGuess: null,
-    });
-  }
+//   handleReset() {
+//     this.setState({
+//       numberToGuess: getRandomNumber(),
+//       numberOfGuesses: 0,
+//       latestGuess: null,
+//     });
+//   }
 
-  render() {
-    const isCorrectGuess = this.state.latestGuess === this.state.numberToGuess;
+//   render() {
+//     const isCorrectGuess = this.state.latestGuess === this.state.numberToGuess;
 
-    const isGameOver =
-      isCorrectGuess || this.state.numberOfGuesses === MAX_ATTEMPTS;
+//     const isGameOver =
+//       isCorrectGuess || this.state.numberOfGuesses === MAX_ATTEMPTS;
 
-    return (
-      <div>
-        <h2>I'm thinking of a number from 1 to 100.</h2>
-        <h2>
-          Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
-        </h2>
-        <GuessControl onGuess={this.handleGuess} />
-        {isGameOver && (
-          <GameOver hasWon={isCorrectGuess} onReset={this.handleReset} />
-        )}
-        {!isGameOver && (
-          <GuessMessage
-            guess={this.state.latestGuess}
-            numberToGuess={this.state.numberToGuess}
-            numberOfGuesses={this.state.numberOfGuesses}
-          />
-        )}
-      </div>
-    );
-  }
-}
+//     return (
+//       <div>
+//         <h2>I'm thinking of a number from 1 to 100.</h2>
+//         <h2>
+//           Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+//         </h2>
+//         <GuessControl onGuess={this.handleGuess} />
+//         {isGameOver && (
+//           <GameOver hasWon={isCorrectGuess} onReset={this.handleReset} />
+//         )}
+//         {!isGameOver && (
+//           <GuessMessage
+//             guess={this.state.latestGuess}
+//             numberToGuess={this.state.numberToGuess}
+//             numberOfGuesses={this.state.numberOfGuesses}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// // export default NumberGuessingGame;
+
+// 12.8 - New functional component
+const NumberGuessingGame = () => {
+  // 12.9 - State variables. reate 3 state variables and their setters for numberToGuess, numberOfGuesses, and latestGuess and initialize them to the same values from the class component version
+  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
+  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+  const [latestGuess, setLatestGuess] = useState(null);
+
+  // 12.10 - Handle a user's guess
+  const handleGuess = (guess) => {
+    setLatestGuess(Number(guess)); // Convert guess to a number
+    setNumberOfGuesses((prev) => prev + 1); // Increment guesses
+  };
+
+  // 12.11 - Reset the game
+  const handleReset = () => {
+    setNumberToGuess(getRandomNumber());
+    setNumberOfGuesses(0);
+    setLatestGuess(null);
+  };
+
+  // 12.12 - Determine if the guess is correct or if the game is over
+  const isCorrectGuess = latestGuess === numberToGuess;
+  const isGameOver = isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS;
+
+  return (
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>
+        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+      </h2>
+      <GuessControl onGuess={handleGuess} />
+      {isGameOver && <GameOver hasWon={isCorrectGuess} onReset={handleReset} />}
+      {!isGameOver && (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NumberGuessingGame;
